fa1690da2e5db34d5c861d82265025d789703d5f
Durgesh Kumar Dwivedi <durgeshdwivedi81@gmail.com>
2023-09-18 21:37:23 +0530

    untracking herosection.jsx

--- src/components/Anime/Carousel/HeroSection.jsx
+++ src/components/Anime/Carousel/HeroSection.jsx
@@ -1,3 +1,43 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:53185b5aeca125d2c6a5a13700a652a935a9cb4ae827fe0c945f77d2076d7b56
-size 1364
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+import './HeroSection.css';
+
+// import required modules
+import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+import AnimeSlides from './Slides/AnimeSlides';
+import MangaSlides from './Slides/MangaSlides';
+import MoviesSlides from './Slides/MoviesSlides';
+
+const HeroSection = () => {
+    return (
+        <>
+            <div className="h-screen">
+                <Swiper
+                    centeredSlides={true}
+                    loop={true}
+                    // autoplay={{
+                    //     delay: 10000,
+                    //     disableOnInteraction: true,
+                    // }}
+                    pagination={{
+                        dynamicBullets: true,
+                    }}
+                    modules={[Autoplay, Pagination, Navigation]}
+                    className="mySwiper"
+                >
+                    <SwiperSlide><AnimeSlides /></SwiperSlide>
+                    <SwiperSlide><MangaSlides /></SwiperSlide>
+                    <SwiperSlide><MoviesSlides /></SwiperSlide>
+                </Swiper>
+            </div>
+        </>
+    )
+}
+
+export default HeroSection
